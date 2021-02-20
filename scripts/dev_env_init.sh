090ba30e3a8fe1f3f24b81cc909c23095ff8a982
shuvava <shuvava@users.noreply.github.com>
2021-02-20 08:52:59 -0800

    improve dev_env_init.sh

--- scripts/dev_env_init.sh
+++ scripts/dev_env_init.sh
@@ -8,7 +8,11 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )/.." >/dev/null 2>&1 && pwd )"
 cd $DIR
-echo "creating default environment variables"
-cp ./scripts/.env.template ./.env
+
+if [! -f .env ]
+then
+    echo "creating default environment variables"
+    cp ./scripts/.env.template ./.env
+fi
 echo "install pip dependencies"
 pipenv install
